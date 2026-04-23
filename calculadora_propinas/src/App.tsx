4555487ee1eea4bedde00e7c3f9116af5d6ddc77
SergioMerinoCortez <sergiodeveloperskate3097@hotmail.com>
2026-04-22 19:29:26 -0600

    paso al video 111

---
+++ calculadora_propinas/src/App.tsx
@@ -0,0 +1,34 @@
+import { menuItems } from "./data/db"
+
+
+function App() {
+
+
+  return (
+    <>
+    <header className="bg-teal-400 py-5">
+    <h1 className="text-center text-4xl font-black">Calculadora de Propinas y Consumo</h1>
+
+    </header>
+      
+    <main className= "max-w-7xl mx-auto mt-20 py-20 grid md:grid-cols-2">
+      <div>
+    <h2>Menú</h2>
+
+      </div>
+    
+      <div>
+    <h2>Consumo</h2>
+      </div>
+    
+    
+    
+    </main>
+
+
+
+          </>
+  )
+}
+
+export default App
